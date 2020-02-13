150fcee077d2015e1ac8a005b3670f4d718dde52
Jalandharp <jpuppala@innominds.com>
2020-02-13 11:19:13 +0530

    # Added initial routing

--- src/server.js
+++ src/server.js
@@ -12,4 +12,19 @@ app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
-export const start = () => {}
+app.get('/getName/:id', (req, res) => {
+    console.log(req.params)
+    res.send({ message: 'hello world for node...!!!' });
+})
+
+app.post('/', (req, res) => {
+    console.log('post body::', req.body);
+    res.send({message: 'okay'});
+})
+
+
+export const start = () => { 
+    app.listen('3000', ()=>{
+        console.log('app started on 300');
+    })
+}
